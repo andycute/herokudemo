e78732dab63afbd946d94ab4d1adfcbd13989287
andy <qingmiyuxuan@gmail.com>
2020-09-26 20:18:08 +0800

    aa

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express().use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.end(123);
-});
+app.get("/", (req, res) => res.render("pages/index"));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
