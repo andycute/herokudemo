cac496d1ff217d60bc5943d93cc74619cdebf8d3
andy <qingmiyuxuan@gmail.com>
2020-09-26 19:55:50 +0800

    aaa

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const app = express();
-app.get("/", function (req, res) {
-  res.end("123");
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${PORT}`);
-});
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
