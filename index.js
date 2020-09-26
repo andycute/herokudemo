9c988a595ecc6b2f19a69815286909b35d9633f7
andy <qingmiyuxuan@gmail.com>
2020-09-26 21:06:40 +0800

    aa

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const app = express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("pages/index"));
+app.get("/", (req, res) => res.render("pages/index", { name: "andy" }));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
